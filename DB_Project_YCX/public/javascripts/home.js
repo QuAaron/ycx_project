663c4ffb508e3b98685d34ab6c09bc501f6f3f08
Yayun Qu <quyayun@gmail.com>
2018-11-28 23:34:39 -0500

    update item purchase page

--- DB_Project_YCX/public/javascripts/home.js
+++ DB_Project_YCX/public/javascripts/home.js
@@ -1,41 +1,39 @@
 var tableData = [
-    [
-        'test1',
-        10,
-        11
-    ],
-    [
-        'test2',
-        20,
-        21
-    ],
-    [
-        'test3',
-        30,
-        31
-    ],
-    [
-        'test3',
-        30,
-        31
-    ],
-    [
-        'test3',
-        30,
-        31
-    ],
-    [
-        'test3',
-        30,
-        31
-    ]
 ]
 var baseUrl = window.location.origin; 
 var showTable = false;
 
 $(document).ready(function() {
-    $('#items-list').DataTable({
+    var table = $('#items-list').DataTable({
+        "autoWidth": false,
+        "columnDefs": [{
+            "targets": 4,
+            "data": null,
+            "width": "20%",
+            "render": function ( data, type, full, meta ) {
+                return "<div class='item-quality-input mdl-textfield mdl-js-textfield mdl-textfield--floating-label'>"
+                +"<input class='mdl-textfield__input' id='" +data[0]+ "' type='text' pattern='-?[0-9]*(\.[0-9]+)?' id='sample4'>"
+                +"<label class='mdl-textfield__label' for='sample4'>Number...</label>"
+                +"<span class='mdl-textfield__error'>Input is not a number!</span></div>"
+            }
+        },
+        {
+            "targets": 5,
+            "data": null,
+            "render": function ( data, type, full, meta ) {
+                return "<button class='mdl-button mdl-js-button mdl-button--icon'>"
+                + "<i class='material-icons'>shopping_cart</i></button>";
+            }
+        }]
     });
+
+    $('#items-list tbody').on( 'click', 'button', function () {
+        var data = table.row( $(this).parents('tr') ).data();
+        console.log(data)
+        purchase(data[0]);
+    } );
+
+    $('#items-table').hide();
 } );
 
 $('#search-input').keyup(function(e){
@@ -46,15 +44,27 @@ $('#search-input').keyup(function(e){
 });
 
 $('#search-input').on("enterKey",function(e){
-    console.log('search')
     if(showTable) {
         $('#items-table').hide();
+        $('#init-msg').show();
     } else {
         $('#items-table').show();
+        $('#init-msg').hide();
     }
     showTable = !showTable;
 });
 
 function logout() {
     window.location.href = baseUrl
+}
+
+function purchase(id) {
+    console.log(id)
+    var quality = $('#'+id).val()
+    if(isNaN(quality) || !quality) {
+        alert("invalid amount")
+    } else {
+        alert("you bought "+quality+ ' '+id)
+    }
+    console.log('purchase')
 }
--- DB_Project_YCX/public/javascripts/main.js
+++ DB_Project_YCX/public/javascripts/main.js
@@ -1,20 +1,4 @@
 var baseUrl = window.location.origin;
-// var mysql = require('mysql')
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'root',
-//   database : 'ycx_schema'
-// });
-// connection.connect()
-
-// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-//   if (err) throw err
-
-//   console.log('The solution is: ', rows[0].solution)
-// })
-
-// connection.end()
 
 function login() {
     var userId = $('#log-in-user-name').val();
@@ -42,7 +26,6 @@ function login() {
         error: function () {
             console.log('http request error')
         }
-
     })
 }
 
